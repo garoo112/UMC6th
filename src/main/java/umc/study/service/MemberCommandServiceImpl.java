8dcacbccf94ad2531f2043b776c59daf5426fcf7
garoo112 <garoo112@gmail.com>
2024-07-01 19:22:54 +0900

    9th

---
+++ src/main/java/umc/study/service/MemberCommandServiceImpl.java
@@ -0,0 +1,50 @@
+package umc.study.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import umc.study.apiPayload.code.status.ErrorStatus;
+import umc.study.apiPayload.exception.handler.FoodCategoryHandler;
+import umc.study.converter.MemberConverter;
+import umc.study.converter.MemberPreferConverter;
+import umc.study.domain.FoodCategory;
+import umc.study.domain.Member;
+import umc.study.domain.mapping.MemberPrefer;
+import umc.study.repository.FoodCategoryRepository;
+import umc.study.repository.MemberRepository;
+import umc.study.web.dto.MemberRequestDTO;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberCommandServiceImpl implements MemberCommandService {
+
+    private final MemberRepository memberRepository;
+
+    private final FoodCategoryRepository foodCategoryRepository;
+
+    @Override
+    @Transactional
+    public Member joinMember(MemberRequestDTO.JoinDto request) {
+
+        Member newMember = MemberConverter.toMember(request);
+        List<FoodCategory> foodCategoryList = request.getPreferCategory().stream()
+                .map(category -> {
+                    return foodCategoryRepository.findById(category)
+                            .orElseThrow(() -> new FoodCategoryHandler(ErrorStatus.FOOD_CATEGORY_NOT_FOUND));
+                }).collect(Collectors.toList());
+
+        List<MemberPrefer> memberPreferList = MemberPreferConverter.toMemberPreferList(foodCategoryList);
+
+        // 양방향 연관 관계 설정은 converter 보단 service 에서 하는 것이 좋음
+        memberPreferList.forEach(memberPrefer -> {
+            memberPrefer.setMember(newMember);
+        });
+
+        return memberRepository.save(newMember);
+    }
+}
---
+++ src/main/java/umc/study/web/controller/MemberRestController.java
@@ -0,0 +1,31 @@
+package umc.study.web.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import umc.study.apiPayload.ApiResponse;
+import umc.study.converter.MemberConverter;
+import umc.study.domain.Member;
+import umc.study.service.MemberCommandService;
+import umc.study.web.dto.MemberRequestDTO;
+import umc.study.web.dto.MemberResponseDTO;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+@RequestMapping("/members")
+public class MemberRestController {
+
+    private final MemberCommandService memberCommandService;
+
+    @PostMapping("/")
+    public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Valid MemberRequestDTO.JoinDto request) {
+        Member member = memberCommandService.joinMember(request);
+        return ApiResponse.onSuccess(MemberConverter.toJoinResultDTO(member));
+    }
+}
